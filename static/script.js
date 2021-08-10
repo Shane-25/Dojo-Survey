8671a10e058ca2c5f38f73d3f7ec06ad42fb3096
Shane Schuring <shane_s91@hotmail.com>
2021-08-09 19:44:55 -0700

    Dojo Survey

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function over(element) {
+    element.style.backgroundColor = "gray";
+    element.style.color = "white";
+}
+    
+function out(element) {
+    element.style.backgroundColor = "orange";
+    element.style.color = "black";
+}
